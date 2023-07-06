b8b638ccfdbb5a627d038bd3b7c260580ac7ccfb
Pia <piabedinicrocci@hotmail.com>
2023-07-05 23:12:21 -0300

    añadido cors

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const cors = require('cors');
 
 var connection = require('./controller/connectionDB.js');
 
@@ -19,6 +19,7 @@ connection.conectar();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
